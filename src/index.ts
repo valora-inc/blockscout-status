9016cfbfc26774aa0d0d790c68a3d78c1fa326ff
Charlie Andrews-Jubelt <cajubelt@users.noreply.github.com>
2023-08-03 16:30:27 -0700

    feat: data freshness check (#2)
    queries for ERC-20 Transfer events from an RPC node for a recent block,
    then queries blockscout and make sure the same event can be found in the
    response. This check could help cLabs create a health check and alerts
    that fire exactly when Valora users are impacted by blockscout falling
    behind

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import yargs from 'yargs'
-import fetch from 'node-fetch'
+import { createPublicClient, http } from 'viem'
+import { celo } from 'viem/chains'
+import got from 'got'
 
 function loadConfig() {
   return yargs
@@ -9,9 +11,24 @@ function loadConfig() {
         type: 'string',
         default: 'https://rc1-blockscout.celo-testnet.org/mainnet',
       },
-      'test-user-address': {
+      'test-token-address': {
         type: 'string',
-        demandOption: true,
+        default: '0x765de816845861e75a25fca122bb6898b8b1282a', // cusd mainnet
+        description:
+          'address of token to check blockscout can find recent transfers for. Should have high transfer ' +
+          'volume to make the test effective (if latest transfers are several blocks back, blockscout could be behind ' +
+          'and still pass the test)',
+      },
+      'max-blocks-behind': {
+        type: 'number',
+        default: 3,
+        description: 'maximum number of blocks behind blockscout should be',
+      },
+      'rpc-url': {
+        type: 'string',
+        description:
+          'URL of the Celo RPC node to use. Should not be blockscout because we use this to cross-check blockscout results',
+        default: 'https://forno.celo.org',
       },
     })
     .parseSync()
@@ -62,20 +79,133 @@ query Transfers($address: AddressHash!, $afterCursor: String) {
 }
 `
 
-export async function main() {
-  const { blockscoutUrl, testUserAddress } = loadConfig()
-
-  // token transfers
-  const tokenTransfersResponse = await fetch(`${blockscoutUrl}/graphql`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-    body: JSON.stringify({
-      query: TRANSFERS_QUERY,
-      variables: { address: testUserAddress },
-    }),
-  })
-
-  return {
-    tokenTransfersOk: tokenTransfersResponse.ok,
+interface BlockscoutTransferResponse {
+  // TODO use zod schema for this?
+  data: {
+    tokenTransferTxs: {
+      edges: {
+        node: {
+          blockNumber: number
+          feeToken: string
+          gasPrice: string
+          gasUsed: string
+          gatewayFee: string
+          gatewayFeeRecipient: string | null
+          input: string
+          timestamp: string
+          transactionHash: string
+          tokenTransfer: {
+            edges: {
+              node: {
+                fromAddressHash: string
+                toAddressHash: string
+                fromAccountHash: string | null
+                toAccountHash: string | null
+                value: string | null
+                tokenAddress: string
+                tokenType: string
+                tokenId: string | null
+              }
+            }[]
+          }
+        }
+      }[]
+      pageInfo: {
+        startCursor: string
+        endCursor: string
+        hasNextPage: boolean
+        hasPreviousPage: boolean
+      }
+    }
   }
 }
+
+export function blockscoutResponseIncludesTransfer(
+  blockscoutResponse: BlockscoutTransferResponse,
+  transferLog: {
+    transactionHash: string
+    args: { from?: string; to?: string; value?: bigint }
+  },
+): boolean {
+  return !!blockscoutResponse.data.tokenTransferTxs.edges.find(
+    (edge) =>
+      edge.node.transactionHash === transferLog.transactionHash &&
+      edge.node.tokenTransfer.edges.find(
+        ({ node: { fromAddressHash, toAddressHash, value } }) =>
+          fromAddressHash.toLowerCase() ===
+            transferLog.args.from?.toLowerCase() &&
+          toAddressHash.toLowerCase() === transferLog.args.to?.toLowerCase() &&
+          value === transferLog.args.value?.toString(),
+      ),
+  )
+}
+
+export async function main() {
+  let error: string | undefined = undefined
+  const { blockscoutUrl, rpcUrl, maxBlocksBehind } = loadConfig()
+
+  const client = createPublicClient({
+    chain: celo,
+    transport: http(rpcUrl),
+  })
+
+  // get recent transfers from the RPC node
+  const lastBlockNumber = await client.getBlockNumber()
+  const fromBlock = lastBlockNumber - BigInt(maxBlocksBehind + 1) // note: this relies on the contract having transfers every block. might need to rethink that.
+  const transferLogs = await client.getLogs({
+    toBlock: lastBlockNumber - BigInt(maxBlocksBehind),
+    fromBlock,
+    event: {
+      anonymous: false,
+      inputs: [
+        { indexed: true, name: 'from', type: 'address' },
+        { indexed: true, name: 'to', type: 'address' },
+        { indexed: false, name: 'value', type: 'uint256' },
+      ],
+      name: 'Transfer',
+      type: 'event',
+    },
+  })
+  if (!transferLogs.length) {
+    throw new Error(`No transfers found in block ${fromBlock}`)
+  }
+  const transferLog = transferLogs[0]
+
+  // verify blockscout can find recent transfers
+  const tokenTransfersResponse = await got
+    .post(`${blockscoutUrl}/graphql`, {
+      // method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      json: {
+        query: TRANSFERS_QUERY,
+        variables: { address: transferLog.args.from },
+      },
+    })
+    .json<BlockscoutTransferResponse>()
+
+  // check that the transfer from the rpc node is in the response
+  const blockscoutIncludesTransfer = blockscoutResponseIncludesTransfer(
+    tokenTransfersResponse,
+    transferLog,
+  )
+  if (!blockscoutIncludesTransfer) {
+    error = `Blockscout does not include transfer with hash ${transferLog.transactionHash} in transfers for user ${transferLog.args.from} from block ${fromBlock}`
+  }
+
+  return {
+    ok: blockscoutIncludesTransfer, // can chain this with other checks for an overall health check
+    error,
+  }
+}
+
+// to test locally, uncomment the following
+// main()
+//   .then((result) => {
+//     console.log(JSON.stringify(result))
+//     console.log('done')
+//     process.exit(0)
+//   })
+//   .catch(console.error)
